905a5ca7d6afcd02a2d48c29bc35b8390310ef2b
donicrosby <donicrosby1995@gmail.com>
2021-05-20 23:29:58 -0400

    Nope.

--- synapse_worker_init/worker_init.py
+++ synapse_worker_init/worker_init.py
@@ -49,7 +49,7 @@ def generate_generic_worker_config(environ,
     }
 
     convert("/templates/synapse_worker.yaml", config_path, volitile_values)
-    with open('config_path', 'r') as f:
+    with open(config_path, 'r') as f:
         print(f.read())
 
 
